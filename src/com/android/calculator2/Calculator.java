bc2f4318220622d5305bd82637eebc3da4b1a5d0
Aayush Gupta <aayushgupta219@gmail.com>
2024-11-02 15:39:51 +0530

    ExactCalculator: Setup for edge to edge support
    Change-Id: Id77e643af7600fac4599d9641e6bf887f18aa18e
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- src/com/android/calculator2/Calculator.java
+++ src/com/android/calculator2/Calculator.java
@@ -62,6 +62,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -315,6 +319,7 @@ public class Calculator extends AppCompatActivity
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_calculator);
+        setupEdgeToEdge();
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
         // Hide all default options in the ActionBar.
@@ -1261,5 +1266,15 @@ public class Calculator extends AppCompatActivity
 
     public interface OnDisplayMemoryOperationsListener {
         boolean shouldDisplayMemory();
+    }
+
+    private void setupEdgeToEdge() {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        ViewCompat.setOnApplyWindowInsetsListener(requireViewById(R.id.main_calculator),
+            (v, windowInsets) -> {
+                Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(0, insets.top, 0, insets.bottom);
+                return WindowInsetsCompat.CONSUMED;
+            });
     }
 }
